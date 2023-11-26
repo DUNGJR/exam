734e7e30a3074479c5a0af63959a7f4540f51b14
DUNGJR <87320646+DUNGJR@users.noreply.github.com>
2023-11-26 20:57:25 +0700

    Delete frontend directory

--- frontend/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const url= "http://localhost:3001/course"
-
-export const fetchCourse = () => axios.get(url);
-
-export const createCourse = (newCourse) => axios.post(url, newCourse);
-
-export const updateCourse = (id, updatedCourse) => axios.patch(`${url}/${id}`, updatedCourse);
--- frontend/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export {default as Navbar} from "./navbar/Navbar";
-export {default as ListenQuiz} from "./quiz-listen/ListenQuiz";
-export {default as Quiz} from "./quiz-reading/ReadingQuiz";
-export {default as Content} from "./content/Content";
-export {default as Course} from "./course/Course";
-export {default as Blog} from "./blog/Blog";
-export {default as Footer} from "./footer/Footer";
-
-
